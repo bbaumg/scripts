b2a7b1658441d6e01c9fe192ba3ff5c1781ad617
bbaumg <bbaumg@gmail.com>
2013-09-28 16:18:31 -0500

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
-#Variables
-log="/var/log/firstboot.log"
-
-# Start the NIC
-echo "Starting the NIC" | tee -a $log
-ifup eth0
-# Install wget - needed to download most current firstrun.sh script
-echo "Installing wget" | tee -a $log
-yum install -y wget
-
-# Download the most current firstrun.sh script
-echo "Downloading firstrun.sh" | tee -a $log
-wget --output-document=/etc/firstrun.sh https://raw.github.com/bbaumg/scripts/master/firstrun.sh
-
-# Start firstrun.sh
-bash /etc/firstrun.sh
+v_firstboot=/etc/firstboot.sh
+echo -n "#" > $v_firstboot
+echo -n ! >> $v_firstboot
+echo "/bin/bash" >> $v_firstboot
+echo "echo \"Starting the NIC\"" >> $v_firstboot
+echo "ifup eth0"  >> $v_firstboot
+echo "echo \"Installing wget\"" >> $v_firstboot
+echo "yum install -y wget" >> $v_firstboot
+echo "echo \"Downloading firstrun.sh\"" >> $v_firstboot
+echo "wget --output-document=/etc/firstrun.sh https://raw.github.com/bbaumg/scripts/master/firstrun.sh" >> $v_firstboot
+echo "bash /etc/firstrun.sh"  >> $v_firstboot
+#cat /etc/firstboot.sh
+logrotate -f /etc/logrotate.conf
+cat /dev/null > /var/log/audit/audit.log
+cat /dev/null > /var/log/wtmp
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+rm -f ~root/.bash_history
+rm -f /etc/ssh/*key*
+rm -rf /etc/ssh/ssh_host_*
+rm -rf /etc/udev/rules.d/70-*
+echo "bash /etc/firstboot.sh"  >> /etc/rc.local
+#cat /etc/rc.local
+unset HISTFILE
+sys-unconfig
