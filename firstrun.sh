7af3751105e78e651e13e0c68c4014a35cb94759
bbaumg <bbaumg@gmail.com>
2013-09-28 16:03:51 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -12,7 +12,7 @@ read admin
 echo "Adding admins" | tee -a $log
 groupadd admins
 useradd --groups admins $admin
-echo “Enter the first admin’s password:”
+echo "Enter the first admin’s password:"
 passwd $admin
 
 # Collecting system information
@@ -32,7 +32,7 @@ dns="8.8.8.8"
 
 #Disable SELINUX
 echo "Disabling SELINUX" | tee -a $log
-sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
 #Secure SSH
 sshconf="/etc/ssh/sshd_config"
@@ -40,41 +40,41 @@ sed -i 's/#PermitRootLogin\ yes/PermitRootLogin\ no/g' $sshconf
 sed -i 's/#Banner\ none/Banner\ \/etc\/issue/g' $sshconf
 
 #Configure IP Tables
-ipt='/etc/sysconfig/iptables'
-echo “*filter” > $ipt
-echo “:INPUT DROP [0:0]” >> $ipt
-echo “:FORWARD DROP [0:0]” >> $ipt
-echo “:OUTPUT ACCEPT [0:0]” >> $ipt
-echo “-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT” >> $ipt
-echo “-A INPUT -p icmp -j ACCEPT” >> $ipt
-echo “-A INPUT -i lo -j ACCEPT” >> $ipt
-echo “-A INPUT -m tcp -p tcp --dport 7 -j ACCEPT” >> $ipt
-echo “#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 22 -j ACCEPT” >> $ipt
-echo “#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1621 -j ACCEPT” >> $ipt
-echo “#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1521 -j ACCEPT” >> $ipt
-echo “# Example SSH Access” >> $ipt
-echo “#-A INPUT -s 69.196.253.30/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT” >> $ipt
-echo “#-A INPUT -s 67.53.65.246/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT” >> $ipt
-echo “” >> $ipt
-echo “#SNMP” >> $ipt
-echo “-A INPUT -p udp --dport 161 -j ACCEPT” >> $ipt
-echo “-A INPUT -p udp --dport 123 -j ACCEPT” >> $ipt
-echo “” >> $ipt
-echo “#Base logging rules” >> $ipt
-echo “-A INPUT -j REJECT --reject-with icmp-host-prohibited” >> $ipt
-echo “-A INPUT -i eth0 -s 10.0.0.0/8 -j LOG --log-prefix "IP DROP SPOOF "” >> $ipt
-echo “-A INPUT -i eth0 -s 172.16.0.0/12 -j LOG --log-prefix "IP DROP SPOOF "” >> $ipt
-echo “-A INPUT -i eth0 -s 192.168.0.0/16 -j LOG --log-prefix "IP DROP SPOOF "” >> $ipt
-echo “-A INPUT -i eth0 -s 224.0.0.0/4 -j LOG --log-prefix "IP DROP MULTICAST "” >> $ipt
-echo “-A INPUT -i eth0 -s 240.0.0.0/5 -j LOG --log-prefix "IP DROP SPOOF "” >> $ipt
-echo “-A INPUT -i eth0 -d 127.0.0.0/8 -j LOG --log-prefix "IP DROP LOOPBACK "” >> $ipt
-echo “-A INPUT -i eth0 -s 169.254.0.0/16  -j LOG --log-prefix "IP DROP MULTICAST "” >> $ipt
-echo “-A INPUT -i eth0 -s 0.0.0.0/8  -j LOG --log-prefix "IP DROP "” >> $ipt
-echo “-A INPUT -i eth0 -s  240.0.0.0/4  -j LOG --log-prefix "IP DROP "” >> $ipt
-echo “-A INPUT -i eth0 -s  255.255.255.255/32  -j LOG --log-prefix "IP DROP  "” >> $ipt
-echo “-A INPUT -i eth0 -s 168.254.0.0/16  -j LOG --log-prefix "IP DROP "” >> $ipt
-echo “-A INPUT -i eth0 -s 248.0.0.0/5  -j LOG --log-prefix "IP DROP "” >> $ipt
-echo “COMMIT” >> $ipt
+ipt="/etc/sysconfig/iptables"
+echo "*filter" > $ipt
+echo ":INPUT DROP [0:0]" >> $ipt
+echo ":FORWARD DROP [0:0]" >> $ipt
+echo ":OUTPUT ACCEPT [0:0]" >> $ipt
+echo "-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT" >> $ipt
+echo "-A INPUT -p icmp -j ACCEPT" >> $ipt
+echo "-A INPUT -i lo -j ACCEPT" >> $ipt
+echo "-A INPUT -m tcp -p tcp --dport 7 -j ACCEPT" >> $ipt
+echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
+echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1621 -j ACCEPT" >> $ipt
+echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1521 -j ACCEPT" >> $ipt
+echo "# Example SSH Access" >> $ipt
+echo "#-A INPUT -s 69.196.253.30/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
+echo "#-A INPUT -s 67.53.65.246/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
+echo "" >> $ipt
+echo "#SNMP" >> $ipt
+echo "-A INPUT -p udp --dport 161 -j ACCEPT" >> $ipt
+echo "-A INPUT -p udp --dport 123 -j ACCEPT" >> $ipt
+echo "" >> $ipt
+echo "#Base logging rules" >> $ipt
+echo "-A INPUT -j REJECT --reject-with icmp-host-prohibited" >> $ipt
+echo "-A INPUT -i eth0 -s 10.0.0.0/8 -j LOG --log-prefix "IP DROP SPOOF "" >> $ipt
+echo "-A INPUT -i eth0 -s 172.16.0.0/12 -j LOG --log-prefix "IP DROP SPOOF "" >> $ipt
+echo "-A INPUT -i eth0 -s 192.168.0.0/16 -j LOG --log-prefix "IP DROP SPOOF "" >> $ipt
+echo "-A INPUT -i eth0 -s 224.0.0.0/4 -j LOG --log-prefix "IP DROP MULTICAST "" >> $ipt
+echo "-A INPUT -i eth0 -s 240.0.0.0/5 -j LOG --log-prefix "IP DROP SPOOF "" >> $ipt
+echo "-A INPUT -i eth0 -d 127.0.0.0/8 -j LOG --log-prefix "IP DROP LOOPBACK "" >> $ipt
+echo "-A INPUT -i eth0 -s 169.254.0.0/16  -j LOG --log-prefix "IP DROP MULTICAST "" >> $ipt
+echo "-A INPUT -i eth0 -s 0.0.0.0/8  -j LOG --log-prefix "IP DROP "" >> $ipt
+echo "-A INPUT -i eth0 -s  240.0.0.0/4  -j LOG --log-prefix "IP DROP "" >> $ipt
+echo "-A INPUT -i eth0 -s  255.255.255.255/32  -j LOG --log-prefix "IP DROP  "" >> $ipt
+echo "-A INPUT -i eth0 -s 168.254.0.0/16  -j LOG --log-prefix "IP DROP "" >> $ipt
+echo "-A INPUT -i eth0 -s 248.0.0.0/5  -j LOG --log-prefix "IP DROP "" >> $ipt
+echo "COMMIT" >> $ipt
 service iptables restart
 
 #Configure Sudoers
@@ -82,7 +82,7 @@ echo "Configuring sudoers" | tee -a $log
 echo "%admins       ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 
 # Write the pre-login banner
-echo “Writing logon banner” | tee -a $log
+echo "Writing logon banner" | tee -a $log
 v_issue=/etc/issue
 echo  > $v_issue
 echo  >> $v_issue
@@ -142,4 +142,3 @@ service network restart
 #Cleanup and reboot
 awk '!/firstboot/' /etc/rc.local
 shutdown -r now
-
