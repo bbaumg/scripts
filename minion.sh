1f028495c4957e9c79380234475d1c023dfe46ff
bbaumg <bbaumg@gmail.com>
2013-09-28 21:47:51 -0500

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-
-echo "Updating issue"
+echo 'Minion is starting to run'
+echo "Updating banner"
 rm -f /etc/issue
 wget --output-document=/etc/issue https://raw.github.com/bbaumg/scripts/master/banner
 cp /etc/issue /etc/issue.$(date +"%Y%m%d%H%M%S")
+
 echo "Updating MOTD"
 rm -f /etc/motd.sh
 wget --output-document=/etc/motd.sh https://raw.github.com/bbaumg/scripts/master/motd.sh
 cp /etc/motd.sh /etc/motd.sh.$(date +"%Y%m%d%H%M%S")
 
-echo "Completed updating scripts"
+echo "YUM update"
+yum update -y
+echo "cleanup installs"
+yum clean all
+
+echo "Minion is done working"
