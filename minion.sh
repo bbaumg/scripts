e0880652d9fd95a412ff16295841a753e0bf09d4
bbaumg <bbaumg@gmail.com>
2013-09-28 21:14:45 -0500

    Update and rename mcp.sh to minion.sh

--- minion.sh
+++ minion.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-v_control="/var/scripts/control"
-cat $v_control
-if [ -f "$v_control" ]; then
-  v_status=`cat $v_control`
-else
-  echo "UPDATE" > $v_control
-fi
-
-if [ "$v_status" = "UPDATE" ]; then
-  wget --output-document=/var/scripts/mcp.sh https://raw.github.com/bbaumg/scripts/master/mcp.sh
-  echo "RUN" > $v_control
-  bash "/var/scripts/mcp.sh"
-  exit 1
-elif [ "$v_status" = "RUN" ]; then
-  exit 1
-fi
-
 
 echo "Updating issue"
 rm -f /etc/issue
