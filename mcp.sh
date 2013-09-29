05b91ddc243948d840f3d75d707d704e8692e5cb
bbaumg <bbaumg@gmail.com>
2013-09-28 21:07:35 -0500

    Update and rename updates.sh to mcp.sh

--- mcp.sh
+++ mcp.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+v_control="/var/scripts/control"
+cat $v_control
+if [ -f "$v_control" ]; then
+  v_status=`cat $v_control`
+else
+  echo "UPDATE" > $v_control
+fi
+
+if [ "$v_status" = "UPDATE" ]; then
+  wget --output-document=/var/scripts/mcp.sh https://raw.github.com/bbaumg/scripts/master/mcp.sh
+  echo "RUN" > $v_control
+  bash "/var/scripts/mcp.sh"
+  exit1
+else if [ "$v_status" = "RUN" ]; then
+
+fi
+
+
+
 echo "Updating issue"
 rm -f /etc/issue
 wget --output-document=/etc/issue https://raw.github.com/bbaumg/scripts/master/banner
