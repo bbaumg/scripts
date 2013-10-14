3fc4ea3e98035f0678f9da6cbb5caf34a9619d8b
bbaumg <bbaumg@gmail.com>
2013-10-13 22:30:02 -0500

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -7,7 +7,7 @@ echo "+   IP Address = $(ifconfig | sed -n '1 p' | awk '{print $1}') $(ifconfig 
 echo "+           OS = $(cat /etc/redhat-release)"
 echo "+          CPU = $(nproc) Cores"
 echo "+       Memory = $(cat /proc/meminfo | sed -n '1 p' |awk '{print $2/1024}' | awk -F . '{print $1}') MB"
-echo "+       Uptime = $(uptime)"
+echo "+       Uptime =$(uptime)"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
 echo
