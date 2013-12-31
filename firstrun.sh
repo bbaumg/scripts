3e41e4f1a54d0d9dac189508b02a7a768ba63373
bbaumg <bbaumg@gmail.com>
2013-12-31 13:22:11 -0600

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -109,7 +109,7 @@ mkdir /var/scripts
 echo '#!/bin/bash' > $v_mpc
 echo 'echo "Starting MCP (Minion Control Program)"' >> $v_mpc
 echo 'echo "Getting the most up to date minion.sh"' >> $v_mpc
-echo 'wget --output-document=/var/scripts/minion.sh https://raw.github.com/bbaumg/scripts/master/minion.sh' >> $v_mpc
+echo 'wget --output-document=/var/scripts/minion.sh https://raw.github.com/bbaumg/scripts/master/minion/minion.sh' >> $v_mpc
 echo 'echo "Run MCP"' >> $v_mpc
 echo 'bash /var/scripts/minion.sh 2>&1 | tee -a /var/log/minion.log' >> $v_mpc
 bash /var/scripts/mcp.sh
