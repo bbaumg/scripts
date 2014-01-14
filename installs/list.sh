0f79967d84a5383c329ddb57524b9ea926ee9741
bbaumg <bbaumg@gmail.com>
2014-01-14 05:59:51 -0600

    Update list.sh

--- installs/list.sh
+++ installs/list.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo 'this is a test'
+
+# List the systems to install 'NAME, URL, LOGFILE)
+
+v_apps[0]="None"
+v_apps[1]='Redmine, https://raw.github.com/bbaumg/scripts/master/installs/redmine2.4.s_my-lo_v1.0.sh, /var/log/app_install_redmine.log'
+v_apps[2]='List, https://raw.github.com/bbaumg/scripts/master/installs/list.sh, /var/log/app_install_list.log'
