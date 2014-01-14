4f811ff523db1883b4ce25557c38377ca2e36edb
bbaumg <bbaumg@gmail.com>
2014-01-14 06:17:38 -0600

    Update list.sh

--- installs/list.sh
+++ installs/list.sh
@@ -10,3 +10,17 @@ for (( i = 0 ; i< ${#v_apps[@]} ; i++ )) do
 done
 echo -en "Should we install an app [0-9]: "
 read v_app
+
+if [ "$v_app" = 0 ]; then
+        echo -e "\nNo applications selected for install\n"
+elif [ -n "$(echo "${v_apps[$v_app]}" | awk -F', ' '{print $1}')" ] && \
+     [ -n "$(echo "${v_apps[$v_app]}" | awk -F', ' '{print $2}')" ] && \
+     [ -n "$(echo "${v_apps[$v_app]}" | awk -F', ' '{print $3}')" ]; then
+        echo -en "Installing $(echo "${v_apps[$v_app]}" | awk -F', ' '{print $1}')...  Please wait while the install script is downloaded\n\n"
+        v_install="$(echo "${v_apps[$v_app]}" | awk -F', ' '{print $2}')"
+        #curl -sL $(echo "${v_apps[$v_app]}" | awk -F', ' '{print $2}') | bash 2>&1 | tee $(echo "${v_apps[$v_app]}" | awk -F', ' '{print $3}')
+else
+        echo -e "\n\n\n\n\n***********************************************************************"
+        echo -e "There was an Error finding the appliation you selected"
+        echo -e "***********************************************************************\n\n\n\n\n"
+fi
