ba5f0b6785df3eae07706d0322a8dc31c63a577e
bbaumg <bbaumg@gmail.com>
2014-01-14 06:21:35 -0600

    Update list.sh

--- installs/list.sh
+++ installs/list.sh
@@ -2,7 +2,7 @@
 # List the systems to install 'NAME, URL, LOGFILE)
 v_apps[0]="None"
 v_apps[1]='Redmine, https://raw.github.com/bbaumg/scripts/master/installs/redmine2.4.s_my-lo_v1.0.sh, /var/log/app_install_redmine.log'
-v_apps[2]='List, https://raw.github.com/bbaumg/scripts/master/installs/list.sh, /var/log/app_install_list.log'
+v_apps[2]='Test, https://raw.github.com/bbaumg/scripts/master/installs/test.sh, /var/log/app_install_test.log'
 
 echo -en "Template Applications:\n"
 for (( i = 0 ; i< ${#v_apps[@]} ; i++ )) do
