20e608968080af8cdf463d2a56db761677b1c145
bbaumg <bbaumg@gmail.com>
2014-01-17 07:24:14 -0600

    Update install.sh

--- installs/install.sh
+++ installs/install.sh
@@ -9,13 +9,11 @@ if [ -f "$log" ]; then
 fi
 
 if [ -f "$install" ]; then
-        echo "Installing"
-        #cat $install
         v_app[0]="$(cat $install | awk -F ', ' '{print $1}')"
         v_app[1]="$(cat $install | awk -F ', ' '{print $2}')"
         v_app[2]="$(cat $install | awk -F ', ' '{print $3}')"
-        #echo "${v_app[0]}"
         echo "Installation selection found...  Installing ${v_app[0]}"
+        bash <(curl -sL "${v_app[1]}") 2>&1 | tee $(echo "${v_apps[3}")
         #curl -sL $(echo "${v_app[1]}" | bash 2>&1 | tee $(echo "${v_apps[2}" | awk -F', ' '{print $3}')"
 else
         echo "Listing"
