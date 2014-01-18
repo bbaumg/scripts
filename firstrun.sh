b3669fe09af21bdcb568fc5631d3ae9cef325916
bbaumg <bbaumg@gmail.com>
2014-01-18 13:08:25 -0600

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -32,7 +32,14 @@ read gateway
 #read dns
 dns="8.8.8.8"
 # Install any apps?
-bash <(curl -sL 'https://raw.github.com/bbaumg/scripts/master/installs/install.sh')
+
+
+
+#bash <(curl -sL 'https://raw.github.com/bbaumg/scripts/master/installs/install.sh')
+
+
+
+
 #echo -n "Template Applications:\n  [1=Redmine]\nShould we install an app [0-9]: "
 #read v_app
 #v_app_1='https://raw.github.com/bbaumg/scripts/master/installs/redmine2.4.s_my-lo_v1.0.sh'
