44b9456219af7b8f1d9cc7b4f8be6fbe0d9131e2
bbaumg <bbaumg@gmail.com>
2014-01-19 07:14:21 -0600

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,20 +1,20 @@
-#v_firstboot=/etc/firstboot.sh
-#echo -n "#" > $v_firstboot
-#echo -n ! >> $v_firstboot
-#echo "/bin/bash" >> $v_firstboot
-#echo "echo \"Starting the NIC\"" >> $v_firstboot
-#echo "ifup eth0"  >> $v_firstboot
-#echo "echo \"Installing wget\"" >> $v_firstboot
-#echo "yum install -y wget" >> $v_firstboot
-#echo "echo \"Downloading firstrun.sh\"" >> $v_firstboot
-#echo "wget --output-document=/etc/firstrun.sh https://raw.github.com/bbaumg/scripts/master/firstrun.sh" >> $v_firstboot
-#echo "bash /etc/firstrun.sh"  >> $v_firstboot
-#cat /etc/firstboot.sh
-#Configure the NIC card
-rc='/etc/rc.local'
-echo "curl -sL https://raw.github.com/bbaumg/scripts/master/firstrun.sh > /etc/firstrun.sh" >> $rc
-echo "chmod 755 /etc/firstrun.sh" >> $rc
-echo "/etc/firstrun.sh" >> $rc
+v_firstboot=/etc/firstboot.sh
+echo -n "#" > $v_firstboot
+echo -n ! >> $v_firstboot
+echo "/bin/bash" >> $v_firstboot
+echo "echo \"Starting the NIC\"" >> $v_firstboot
+echo "ifup eth0"  >> $v_firstboot
+echo "echo \"Installing wget\"" >> $v_firstboot
+echo "yum install -y wget" >> $v_firstboot
+echo "echo \"Downloading firstrun.sh\"" >> $v_firstboot
+echo "wget --output-document=/etc/firstrun.sh https://raw.github.com/bbaumg/scripts/master/firstrun.sh" >> $v_firstboot
+echo "bash /etc/firstrun.sh"  >> $v_firstboot
+cat /etc/firstboot.sh
+Configure the NIC card
+#rc='/etc/rc.local'
+#echo "curl -sL https://raw.github.com/bbaumg/scripts/master/firstrun.sh > /etc/firstrun.sh" >> $rc
+#echo "chmod 755 /etc/firstrun.sh" >> $rc
+#echo "/etc/firstrun.sh" >> $rc
 
 #echo "bash <(curl -sL https://raw.github.com/bbaumg/scripts/master/firstrun.sh)" >> $rc
 
@@ -33,7 +33,7 @@ rm -f ~root/.bash_history
 rm -f /etc/ssh/*key*
 rm -rf /etc/ssh/ssh_host_*
 rm -rf /etc/udev/rules.d/70-*
-#echo "bash /etc/firstboot.sh"  >> /etc/rc.local
+echo "bash /etc/firstboot.sh"  >> /etc/rc.local
 #cat /etc/rc.local
 unset HISTFILE
 #sys-unconfig
