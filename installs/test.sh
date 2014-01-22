b6491a8d8364f15f1ef071b9c36ed6d2cfa7eb33
bbaumg <bbaumg@gmail.com>
2014-01-21 20:46:45 -0600

    Update test.sh

--- installs/test.sh
+++ installs/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "installing Test..   Woot"
 echo $1
-if [ -n "$1" ]; then echo "now is when I would reboot"; fi
-if [ "$1" == 'true' ]; then echo "rebooting"; fi
+if [ "$1" != 'firstboot' ]; then echo "rebooting"; fi
