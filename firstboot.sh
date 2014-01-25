d2eb439b4327834686c8a39fe1b0b9fde3ab3014
bbaumg <bbaumg@gmail.com>
2014-01-25 08:53:30 -0600

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,17 +1,4 @@
-#v_firstboot=/etc/firstboot.sh
-#echo -n "#" > $v_firstboot
-#echo -n ! >> $v_firstboot
-#echo "/bin/bash" >> $v_firstboot
-#echo "echo \"Starting the NIC\"" >> $v_firstboot
-#echo "ifup eth0"  >> $v_firstboot
-#echo "echo \"Installing wget\"" >> $v_firstboot
-#echo "yum install -y wget" >> $v_firstboot
-#echo "echo \"Downloading firstrun.sh\"" >> $v_firstboot
-#echo "wget --output-document=/etc/firstrun.sh https://raw.github.com/bbaumg/scripts/master/firstrun.sh" >> $v_firstboot
-#echo "bash /etc/firstrun.sh"  >> $v_firstboot
-#cat /etc/firstboot.sh
-#echo "bash /etc/firstboot.sh"  >> /etc/rc.local
-#cat /etc/rc.local
+#!/bin/bash
 
 rc='/etc/rc.local'
 sed -i --follow-symlinks '/firstboot/d' $rc
