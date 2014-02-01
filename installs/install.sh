2faf79990741df593bdc98eeb29722d5079bf8bd
bbaumg <bbaumg@gmail.com>
2014-02-01 07:56:18 -0600

    Update install.sh

--- installs/install.sh
+++ installs/install.sh
@@ -6,7 +6,7 @@ install="$c_dir/install.run"
 c_repo="https://raw.github.com/bbaumg/scripts/master"
 #if [ -f "$log" ]; then exit 1; fi
 
-source <(curl -srL "$c_repo/minion/functions")
+source <(curl -srL "$c_repo/minion/functions.sh")
 
 if [ -f "$install" ]; then
         v_app[0]="$(cat $install | awk -F ', ' '{print $1}')"
