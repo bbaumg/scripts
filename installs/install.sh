e59be00faf2df4a75ccc288956af620c35429108
bbaumg <bbaumg@gmail.com>
2014-02-01 07:19:10 -0600

    Update install.sh

--- installs/install.sh
+++ installs/install.sh
@@ -3,21 +3,21 @@
 #log="/var/log/installs.log"
 c_dir='/var/scripts/installs'
 install="$c_dir/install.run"
-c_repo="https://raw.github.com/bbaumg/scripts/master/installs"
+c_repo="https://raw.github.com/bbaumg/scripts/master"
 #if [ -f "$log" ]; then exit 1; fi
+
+source "$c_repo/minion/functions"
 
 if [ -f "$install" ]; then
         v_app[0]="$(cat $install | awk -F ', ' '{print $1}')"
         v_app[1]="$(cat $install | awk -F ', ' '{print $2}')"
         v_app[2]="$(cat $install | awk -F ', ' '{print $3}')"
-        echo "Installation selection found...  Installing ${v_app[0]}"
-        #awk '!/install.sh/' /etc/rc.local > /etc/rc.local.tmp && mv -f /etc/rc.local.tmp /etc/rc.local
+        logger "'Installation selection found...  Installing ${v_app[0]}'" $c_dir/${v_app[2]}
         sed -i --follow-symlinks '/install.sh/d' /etc/rc.local
         rm -f $install
         bash <(curl -sL ${v_app[1]}) 2>&1 | tee $c_dir/${v_app[2]}
-        #curl -sL $(echo "${v_app[1]}" | bash 2>&1 | tee $(echo "${v_apps[2}" | awk -F', ' '{print $3}')"
 else
-        source <(curl -sL "$c_repo/list.sh")
+        source <(curl -sL "$c_repo/installs/list.sh")
         if [ -z ${v_apps[0]} ]; then
                 echo -e "\n\n\n\n\n***********************************************************************"
                 echo -e "Unable to load the list of installation options"
@@ -41,14 +41,16 @@ else
                 mkdir -p $c_dir
                 echo "${v_apps[$v_app]}" > $install
                 rc='/etc/rc.local'
-                echo "curl -sL $c_repo/install.sh > $c_dir/install.sh" >> $rc
+                echo "curl -sL $c_repo/installs/install.sh > $c_dir/install.sh" >> $rc
                 echo "chmod 755 $c_dir/install.sh" >> $rc
                 echo "bash $c_dir/install.sh" >> $rc
 #                echo "bash <(curl -sL $c_repo/install.sh)" >> /etc/rc.local
 #                echo "Installation will begin when system resumes from the next reboot"
-                if [ "$1" != 'firstboot' ]; then echo -n "System is rebooting now...  "; fi
-                sleep 3
-                if [ "$1" != 'firstboot' ]; then reboot; fi
+                if [ "$1" != 'firstboot' ]; then 
+                        echo -n "System is rebooting now...  "
+                        sleep 3
+                        reboot
+                fi
         else
                 echo -e "\n\n\n\n\n***********************************************************************"
                 echo -e "There was an Error finding the appliation you selected"
