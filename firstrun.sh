169fe40b704c54e83ca0ad69cc4c152b563340dc
bbaumg <bbaumg@gmail.com>
2014-01-31 22:52:11 -0600

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -20,8 +20,7 @@ until [ $val_admin ]; do
 		val_admin=true
 		echo -en "Admins group and default admin creation:" success
 	else
-		echo -en "Admins group and default admin creation:"
-		failure
+		echo -en "Admins group and default admin creation:" failure
 	fi
 done
 
