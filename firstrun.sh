a66a9510aa0e1420cd853f13cdcaba6d2c39ea75
bbaumg <bbaumg@gmail.com>
2014-02-08 16:50:31 -0600

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -6,6 +6,7 @@ v_appinstall_url='https://raw.github.com/bbaumg/scripts/master/installs/install.
 v_defaultapps='logrotate bind-utils cifs-utils vim openssh-clients wget ntsysv ntp traceroute lynx ftp sudoers curl git'
 
 # Verify it has not run before
+if [ "$1" == "test" ]; then rm -f "$log"; fi
 if [ -f "$log" ]; then exit 1; fi
 source /etc/init.d/functions
 clear
