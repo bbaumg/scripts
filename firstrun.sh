2f28f40b094fb5da60e147116093293c133f4de3
bbaumg <bbaumg@gmail.com>
2014-02-08 16:28:42 -0600

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -71,9 +71,8 @@ until [ "$val_allgood" == "YES" ]; do
 	until [ "$val_gateway" == 1 ]; do
 	        #echo -n "Enter the gateway [ENTER]: "
 	        if [ "$gateway" == '' ]; then 
-iparray=(`echo $ipaddr | tr "." " "`)
-gateway="${iparray[0]}.${iparray[1]}.${iparray[2]}."
-echo "$gateway"
+			iparray=(`echo $ipaddr | tr "." " "`)
+			gateway="${iparray[0]}.${iparray[1]}.${iparray[2]}.1"
 	        fi
 	        read -e -p "Enter the gateway [ENTER]: " -i "$gateway" gateway
 	        if [[ ! $gateway =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
@@ -134,32 +133,16 @@ echo "-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT" >> $ipt
 echo "-A INPUT -p icmp -j ACCEPT" >> $ipt
 echo "-A INPUT -i lo -j ACCEPT" >> $ipt
 echo "-A INPUT -m tcp -p tcp --dport 7 -j ACCEPT" >> $ipt
-echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
 echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1621 -j ACCEPT" >> $ipt
 echo "#-A INPUT -s 172.16.0.0/16 -m state --state NEW -p tcp --dport 1521 -j ACCEPT" >> $ipt
+echo "# Open SSH to local subnet" >> $ipt
+echo "-A INPUT -s ${iparray[0]}.${iparray[1]}.${iparray[2]}.0/24 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
 echo "# Example SSH Access" >> $ipt
-echo "#-A INPUT -s 69.196.253.30/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
-echo "#-A INPUT -s 67.53.65.246/32 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
-echo "-A INPUT -s 10.15.81.0/24 -m state --state NEW -p tcp --dport 22 -j ACCEPT" >> $ipt
 echo "" >> $ipt
 echo "#SNMP" >> $ipt
 echo "-A INPUT -p udp --dport 161 -j ACCEPT" >> $ipt
 echo "-A INPUT -p udp --dport 123 -j ACCEPT" >> $ipt
 echo "" >> $ipt
-echo "#Base logging rules" >> $ipt
-echo "-A INPUT -j REJECT --reject-with icmp-host-prohibited" >> $ipt
-echo "-A INPUT -i eth0 -s 10.0.0.0/8 -j LOG --log-prefix \"IP DROP SPOOF \"" >> $ipt
-echo "-A INPUT -i eth0 -s 172.16.0.0/12 -j LOG --log-prefix \"IP DROP SPOOF \"" >> $ipt
-echo "-A INPUT -i eth0 -s 192.168.0.0/16 -j LOG --log-prefix \"IP DROP SPOOF \"" >> $ipt
-echo "-A INPUT -i eth0 -s 224.0.0.0/4 -j LOG --log-prefix \"IP DROP MULTICAST \"" >> $ipt
-echo "-A INPUT -i eth0 -s 240.0.0.0/5 -j LOG --log-prefix \"IP DROP SPOOF \"" >> $ipt
-echo "-A INPUT -i eth0 -d 127.0.0.0/8 -j LOG --log-prefix \"IP DROP LOOPBACK \"" >> $ipt
-echo "-A INPUT -i eth0 -s 169.254.0.0/16  -j LOG --log-prefix \"IP DROP MULTICAST \"" >> $ipt
-echo "-A INPUT -i eth0 -s 0.0.0.0/8  -j LOG --log-prefix \"IP DROP \"" >> $ipt
-echo "-A INPUT -i eth0 -s 240.0.0.0/4  -j LOG --log-prefix \"IP DROP \"" >> $ipt
-echo "-A INPUT -i eth0 -s 255.255.255.255/32  -j LOG --log-prefix \"IP DROP \"" >> $ipt
-echo "-A INPUT -i eth0 -s 168.254.0.0/16  -j LOG --log-prefix \"IP DROP \"" >> $ipt
-echo "-A INPUT -i eth0 -s 248.0.0.0/5  -j LOG --log-prefix \"IP DROP \"" >> $ipt
 echo "COMMIT" >> $ipt
 service iptables restart
 
