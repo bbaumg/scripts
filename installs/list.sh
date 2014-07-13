66947caada040140163583faaceaa1567110e682
bbaumg <bbaumg@gmail.com>
2014-07-13 09:12:07 -0500

    Update list.sh

--- installs/list.sh
+++ installs/list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # List the systems to install 'NAME, URL, LOGFILE)
 v_apps[0]="None"
-v_apps[1]='Redmine, https://raw.github.com/bbaumg/scripts/master/installs/redmine2.4.s_my-lo_v1.0.sh, app_install_redmine.log'
-v_apps[2]='Test, https://raw.github.com/bbaumg/scripts/master/installs/test.sh, app_install_test.log'
-v_apps[3]='Wordpress, https://raw.github.com/bbaumg/scripts/master/installs/wordpress.sh, app_install_wordpress.log'
+v_apps[1]='Redmine, https://raw.githubusercontent.com/bbaumg/scripts/master/installs/redmine2.4.s_my-lo_v1.0.sh, app_install_redmine.log'
+v_apps[2]='Test, https://raw.githubusercontent.com/bbaumg/scripts/master/installs/test.sh, app_install_test.log'
+v_apps[3]='Wordpress, https://raw.githubusercontent.com/bbaumg/scripts/master/installs/wordpress.sh, app_install_wordpress.log'
