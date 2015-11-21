621b931e29c60b962c61b0e27ba33b0b26a846b9
bbaumg <bbaumg@gmail.com>
2015-11-20 22:36:09 -0600

    Made OS independent

--- kickstart/motd.sh
+++ kickstart/motd.sh
@@ -4,7 +4,7 @@ echo
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+     Hostname = $(hostname)"
 echo "+   IP Address = $(ifconfig | sed -n '1 p' | awk '{print $1}') $(ifconfig | sed -n '2 p' | awk '{print $2}')"
-echo "+           OS = $(cat /etc/redhat-release)"
+echo "+           OS = $(cat /etc/*-release | grep PRETTY_NAME | awk -F\= '{gsub(/"/,"",$2);print $2}')"
 echo "+          CPU = $(nproc) Core(s)"
 echo "+       Memory = $(cat /proc/meminfo | sed -n '1 p' |awk '{print $2/1024}' | awk -F . '{print $1}') MB"
 echo "+       Uptime =$(uptime)"
