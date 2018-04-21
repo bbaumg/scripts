67126820308e3186401d146fdbf437ada1304f5e
bbaumg <bbaumg@gmail.com>
2018-04-21 08:44:49 -0500

    Update motd.sh

--- kickstart/motd.sh
+++ kickstart/motd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+entname=$(ip addr | awk -F ": " '!/  /{print $2}' | grep --invert-match 'lo')
 echo
-echo 
+echo
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+     Hostname = $(hostname)"
-echo "+   IP Address = $(ifconfig | sed -n '1 p' | awk '{print $1}') $(ifconfig | sed -n '2 p' | awk '{print $2}')"
-echo "+           OS = $(cat /etc/*-release | grep PRETTY_NAME | awk -F\= '{gsub(/"/,"",$2);print $2}')"
+#echo "+   IP Address = $(ifconfig | sed -n '1 p' | awk '{print $1}') $(ifconfig | sed -n '2 p' | awk '{print $2}')"
+echo "+   IP Address = $(ip addr show $entname | grep 'inet ' | awk '{print $2}') on $entname"
+echo "+           OS = $(cat /etc/system-release)"
 echo "+          CPU = $(nproc) Core(s)"
 echo "+       Memory = $(cat /proc/meminfo | sed -n '1 p' |awk '{print $2/1024}' | awk -F . '{print $1}') MB"
 echo "+       Uptime =$(uptime)"
