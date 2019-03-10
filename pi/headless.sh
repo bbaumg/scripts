d8f0bc18643358cd03c9489e3f6261f255b4b818
Barrett <bbaumg@gmail.com>
2019-03-10 09:46:11 -0500

    build headless.sh

--- pi/headless.sh
+++ pi/headless.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Usage
+#curl https://raw.githubusercontent.com/bbaumg/scripts/master/pi/headless.sh > headless.sh
+#bash headless.sh
+
 #based on information learned from
 #https://core-electronics.com.au/tutorials/raspberry-pi-zerow-headless-wifi-setup.html
 
@@ -26,7 +30,7 @@ while true; do
             #echo -en "\n$wifiSSID\n"
             echo -en "What is the Password? "
             read -s wifiPass
-            echo -en "\nCreating wifi settings file 'wpa_supplicant.conf'"
+            echo -en "\nCreating wifi settings file 'wpa_supplicant.conf'\n"
             #echo -en "\n$wifiPass\n"
             echo -en "country=US\n"\
 "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n"\
