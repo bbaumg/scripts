8605d5d888e9278e2fab737f1a8b9b87e18c305e
bbaumg <bbaumg@gmail.com>
2022-04-03 11:05:48 -0500

    Update build.sh

--- pi/build.sh
+++ pi/build.sh
@@ -52,6 +52,26 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+while true; do
+    read -p "Do you want to setup wireless (y/n)? " yn
+    case $yn in
+        [Yy]* )
+            read -p "What SSID do you want to connect to? " wifiSSID
+            #echo -en "\n$wifiSSID\n"
+            echo -en "What is the Password? "
+            read -s wifiPass
+            echo -en "\nCreating wifi settings file 'wpa_supplicant.conf'\n"
+            #echo -en "\n$wifiPass\n"
+            echo -en "country=US\n"\
+"ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n"\
+"network={\n\tssid=\"$wifiSSID\"\n\tpsk=\"$wifiPass\"\n\tkey_mgmt=WPA-PSK\n}\n" > wpa_supplicant.conf
+            break;;
+        [Nn]* )
+                echo "OK...  No Wireless"
+                break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 # Change the password
 echo "Change your password" | tee -a $log
