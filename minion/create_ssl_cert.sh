1659b97eb7fbead67b0ba3a861a55f9fd2f8d854
bbaumg <bbaumg@gmail.com>
2024-02-24 08:54:23 -0600

    Delete minion directory

--- minion/create_ssl_cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Set a name for the certificate [ENTER]: "
-read v_keyname
-openssl genrsa -out /etc/pki/tls/private/$v_keyname.key 1024
-openssl req -new -key /etc/pki/tls/private/$v_keyname.key -out /etc/pki/tls/private/$v_keyname.csr
-openssl x509 -req -days 365 -in /etc/pki/tls/private/$v_keyname.csr -signkey /etc/pki/tls/private/$v_keyname.key -out /etc/pki/tls/certs/$v_keyname.crt
-echo ""
-echo "SSLCertificateFile /etc/pki/tls/private/$v_keyname.key"
-echo "SSLCertificateKeyFile /etc/pki/tls/certs/$v_keyname.crt"
--- minion/functions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#  
-#  This contains all of the funtions used by other parts of the scripting system.
-
-logger () { 
-  echo -e "\n==============================================================================\n$(date)  $1" | tee -a $v_log
-}
