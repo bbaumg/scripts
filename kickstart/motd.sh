9c09e916aab06c4d17d927b89a6ed70ccde9813e
bbaumg <bbaumg@gmail.com>
2025-04-06 12:18:07 -0500

    Update motd.sh

--- kickstart/motd.sh
+++ kickstart/motd.sh
@@ -3,6 +3,10 @@ echo
 echo
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+     Hostname = $(hostname)"
+v_pimodel=$(cat /proc/cpuinfo | grep 'Model' | awk '{for (i=3; i <= NF; i++) printf("%s ", $i); print ""}')
+if [ "$v_pimodel" != "" ]; then
+	echo "+     Pi Model = $v_pimodel"
+fi
 echo "+   IP Address = $(ip --brief address | awk '/UP/ {print $3, "on", $1}')"
 echo "+           OS = $(cat /etc/*-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/"//g')"
 echo "+          CPU = $(nproc) Core(s)"
